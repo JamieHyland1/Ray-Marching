d948e975286d6f70207313a1ba7395525fbebc0b
I506589 <jamie.hyland@sap.com>
2019-05-16 11:35:51 +0100

    getShortestDistance from ray to buildings function

--- js/Ray.js
+++ js/Ray.js
@@ -7,7 +7,16 @@ class Ray{
     noStroke();
     ellipse(this.pos.x, this.pos.y,50,50)
   }
-
+  //Refactor buildings to user vectors to make it easier to draw line to shortest building
+  getShortestDistance(buildings){
+      let minDistance = 0;
+      let currentDistance = 0;
+      for(let building in buildings){
+        currentDistance = sqrt(pow((building.x - this.pos.x),2) + pow((building.y-this.pos.y),2))-50;
+        if(currentDistance < minDistance) minDistance = currentDistance;
+      }
+      return minDistance
+  }
   update(x,y){
     this.pos.x = x;
     this.pos.y = y;
