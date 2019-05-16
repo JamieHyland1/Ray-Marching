62f2247d8db698b4eec4a8264f32cb1df9017edd
I506589 <jamie.hyland@sap.com>
2019-05-16 15:35:17 +0100

    reWrote distance function and added castRay() function

--- js/Ray.js
+++ js/Ray.js
@@ -1,21 +1,32 @@
 class Ray{
   constructor(x,y){
     this.pos = createVector(x,y)
+    this.shortestDisance;
+    this.test;
   }
   show(){
-    fill(255);
-    noStroke();
-    ellipse(this.pos.x, this.pos.y,50,50)
+    stroke(255)
+    fill(255)
+    ellipse(this.pos.x, this.pos.y,25,25)
+
   }
-  //Refactor buildings to user vectors to make it easier to draw line to shortest building
   getShortestDistance(buildings){
-      let minDistance = 0;
       let currentDistance = 0;
-      for(let building in buildings){
-        currentDistance = sqrt(pow((building.x - this.pos.x),2) + pow((building.y-this.pos.y),2))-50;
-        if(currentDistance < minDistance) minDistance = currentDistance;
+      let minDistance = 10000000;
+      for(let building of buildings){
+          currentDistance = sqrt(pow((building.pos.x-this.pos.x),2)+pow((building.pos.y-this.pos.y),2))-building.size/2;
+          if(currentDistance < minDistance){
+              minDistance = currentDistance
+              this.shortestDisance = p5.Vector.sub(this.pos,building.pos)
+          }
       }
-      return minDistance
+
+      this.test =  minDistance
+  }
+  castRay(){
+    fill(255,255,255,100)
+    ellipse(this.pos.x,this.pos.y,this.test*2,this.test*2)
+    //line(this.pos.x,this.pos.y,this.shortestDisance.x*-1,-this.shortestDisance.y)
   }
   update(x,y){
     this.pos.x = x;
