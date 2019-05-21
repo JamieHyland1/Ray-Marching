e663d0357373f123f00648553f2b4ff8d5f296e1
I506589 <jamie.hyland@sap.com>
2019-05-21 14:07:54 +0100

    current sketch

--- js/sketch.js
+++ js/sketch.js
@@ -1,14 +1,16 @@
 let buildings = []
-
-let ray;
+let angle= 0;
+let size
+let eye;
 function setup() {
    createCanvas(displayWidth,720);
    background(52);
-   ray = new Ray(50,50)
-   for(var i = 0; i < 5; i++){
-     let size = random(100,200)
-     buildings.push(new Building(random((displayWidth/3)-size,displayWidth),random(720-size),size, 'CIRCLE'))
-   }
+   eye = new Eye(50,50)
+   // for(var i = 0; i < 15; i++){
+   //    size = random(60,200)
+   //   buildings.push(new Building(random((displayWidth/3)-size,displayWidth),random(720-size),size, 'CIRCLE'))
+   // }
+
 }
 
 function draw() {
@@ -16,8 +18,14 @@ function draw() {
     for(let building of buildings){
         building.show();
     }
-    ray.update(mouseX,mouseY);
-    console.log(ray.getShortestDistance(buildings))
-    ray.show();
-    ray.castRay();
+    eye.update(50,mouseY)
+    eye.castRays(buildings);
+    eye.show();
+
+
+}
+function mousePressed(){
+  size = random(60,200)
+
+  buildings.push(new Building(mouseX, mouseY, size, 'CIRCLE'))
 }
